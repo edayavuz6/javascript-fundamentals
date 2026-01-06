819a5aea4af5d5614366ddc9a622d4043cce818f
Eda Yavuz <contactyeda6@gmail.com>
2026-01-06 17:48:50 +0300

    Add grading logic with if-elseif-else structure

---
+++ if-elseif-else.js
@@ -0,0 +1,11 @@
+let grade = 75;
+
+if (grade >= 85) {
+  console.log("Excellent");
+} else if (grade >= 70) {
+  console.log("Good");
+} else if (grade >= 50) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
