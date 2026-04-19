3f8568a34a9027344d1d2995ed10840eb4cecba7
Eda Yavuz <contactyeda6@gmail.com>
2026-04-19 15:33:44 +0300

    Add asynchronous JavaScript example
    Demonstrating asynchronous execution in JavaScript using setTimeout.

---
+++ asynchronous-example.js
@@ -0,0 +1,10 @@
+// ASYNCHRONOUS Example
+
+console.log("1. İşlem başladı");
+
+// Asenkron işlem (gecikmeli)
+setTimeout(() => {
+  console.log("3. Asenkron işlem tamamlandı");
+}, 2000);
+
+console.log("2. İşlem devam ediyor");
